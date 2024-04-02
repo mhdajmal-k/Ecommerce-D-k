54e8a41fa1a83275bf82213a6b8bf7966b50457f
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-04-02 21:44:19 +0530

    lading page value changed

--- configure/dbconnect.js
+++ configure/dbconnect.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 
 const dbConnect=(()=>{
     
-    mongoose.connect("mongodb+srv://ajmalchundappuram:q0sj5VZoentLmMxd@duckandwave.zsjjlfm.mongodb.net/duck&wave?retryWrites=true&w=majority&appName=duckandwave",{ useNewUrlParser: true }).then(()=>{
+    mongoose.connect(process.env.db2,{ useNewUrlParser: true }).then(()=>{
         console.log("db connected Successfully...")
     })
     .catch((err)=>{
